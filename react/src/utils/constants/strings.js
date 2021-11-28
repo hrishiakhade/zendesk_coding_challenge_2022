af412d9ca849e32438101adb8e71f0be9eeb703d
Hrushikesh <hrishakhade@gmail.com>
2021-11-27 22:13:29 -0500

    added loader

--- react/src/utils/constants/strings.js
+++ react/src/utils/constants/strings.js
@@ -1,6 +1,6 @@
 const strings = {
     network_error_header: "Network Error",
-    network_api_error_msg:"Network Error, check that the server is running and refresh the page.",
+    network_api_error_msg:"Network Error, Please check if the server is running and reload the page again.",
     network_auth_error: "Couldn't Authenticate You.",
     network_api_error: "API endpoint not found.",
     network_something_wrong:"Oops, Something went Wrong !!",
